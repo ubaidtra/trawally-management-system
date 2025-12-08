ffbf2c5d9cf54cf9e38d373e80ca58029c86ae8f
ubaidtra <ubaidtr@gmail.com>
2025-12-08 19:46:26 +0000

    Revert "Enable CEO to review all contracts and services in detail with comprehensive information"
    This reverts commit 42291aab4bf0428171911e3c74b90e87e29a60ba.

--- routes/ceo.js
+++ routes/ceo.js
@@ -11,16 +11,6 @@ router.use(checkRole('ceo'));
 router.get('/dashboard', ceoController.showDashboard);
 router.get('/reports', ceoController.showReports);
 
-router.get('/contracts', ceoController.showContracts);
-router.get('/contracts/:id', ceoController.showContractDetails);
-router.get('/contracts/:id/invoice', invoiceController.printContractInvoice);
-router.get('/contracts/:id/receipt', invoiceController.printContractReceipt);
-
-router.get('/services', ceoController.showServices);
-router.get('/services/:id', ceoController.showServiceDetails);
-router.get('/services/:id/invoice', invoiceController.printServiceInvoice);
-router.get('/services/:id/receipt', invoiceController.printServiceReceipt);
-
 router.get('/messages', messageController.showMessages);
 router.post('/messages', messageController.sendMessage);
 router.patch('/messages/:id/read', messageController.markAsRead);
